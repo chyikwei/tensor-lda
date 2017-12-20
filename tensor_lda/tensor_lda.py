7d8d4360e73b0191bd72895a8822122868432a60
Chyi-Kwei Yau <chyikwei.yau@gmail.com>
2017-12-20 15:40:24 -0500

    add scikit-learn contrib template to project

---
+++ tensor_lda/tensor_lda.py
@@ -0,0 +1,28 @@
+"""Latent Dirichlet Allocation with tesnsor Decomposition
+
+TODO: add short desc
+"""
+
+# Author: Chyi-Kwei Yau
+
+from __future__ import print_function
+
+import numpy as np
+import scipy.sparse as sp
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class TensorLDA(BaseEstimator, TransformerMixin):
+	"""
+	"""
+
+	def __init__(self):
+		pass
+
+	def fit(self, X, y=None):
+		pass
+
+	def transform(self, X):
+		pass
+
---
+++ tensor_lda/tests/test_tensor_lda.py
@@ -0,0 +1,9 @@
+from tensor_lda.tensor_lda import TensorLDA
+
+
+def test_fake():
+	t = TensorLDA()
+
+#def test_estimator():
+#    """Test estimator"""
+#    return check_estimator(TensorLDA)
