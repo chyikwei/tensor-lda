1f7e4a42ac28d593027bda1443859bb592bfb9bc
Chyi-Kwei Yau <chyikwei.yau@gmail.com>
2017-12-27 00:08:04 -0500

    add cooccurrence_expectation

--- tensor_lda/moments.py
+++ tensor_lda/moments.py
@@ -68,6 +68,122 @@ def first_order_moments(X, min_words, whom):
 
         for w_id, w_cnt in zip(ids, cnts):
             e1[w_id] += (w_cnt / float(words_cnt))
-    e1 /= n_samples
+    e1 /= (n_samples - ignored_docs)
 
     return (e1, ignored_docs)
+
+
+def cooccurrence_expectation(X, min_words, whom, batch_size=1000):
+    """Expectation of Word Co-occurrence
+
+    Expectation of 2 words occured in the same document.
+
+    Parameters
+    ----------
+    X : array-like or sparse matrix, shape=(n_samples, n_features)
+        Matrix of Document-word frequency. `n_samples` is the
+        number of documnet and `n_features` are number of unique
+        words in the corpus.
+    
+    min_words : Integer
+        Minimum number of words in each document. In LDA, the number
+        is 3 since we need 3rd order moments.
+
+    whom : string
+        which method called this function.
+
+    Returns
+    -------
+    e2 : sparse array, shape=(n_features, n_features)
+        Expectation of word pairs
+    
+    ignored: integer
+        Number of ignored documents.
+
+    """
+
+    X = check_array(X, accept_sparse='csr')
+    check_non_negative(X, whom)
+    n_samples, n_features = X.shape
+    is_sparse_x = sp.issparse(X)
+
+    pairs = []
+    doc_word_cnts = np.squeeze(np.asarray(X.sum(axis=1))).reshape(n_samples,)
+    ignored_docs = 0
+
+    if is_sparse_x:
+        X_data = X.data
+        X_indices = X.indices
+        X_indptr = X.indptr
+
+    # TODO: optimize for loop with cython
+    e2 = sp.coo_matrix((n_features, n_features))
+    rows = []
+    cols = []
+    vals = []
+    for idx_d in xrange(n_samples):
+        # get word_id and count in each document
+        words_cnt = doc_word_cnts[idx_d]
+        if words_cnt < min_words:
+            ignored_docs += 1
+            continue
+
+        if is_sparse_x:
+            ids = X_indices[X_indptr[idx_d]:X_indptr[idx_d + 1]]
+            cnts = X_data[X_indptr[idx_d]:X_indptr[idx_d + 1]]
+        else:
+            ids = np.nonzero(X[idx_d, :])[0]
+            cnts = X[idx_d, ids]
+        unique_word_cnt = len(ids)
+        unique_pairs = unique_word_cnt * (unique_word_cnt - 1)
+        # index for total tokens are:
+        # 0 to (unique_word_cnt - 1): pair(i, i)
+        # unique_word_cnt to end: pair(i, j) where (j > i)
+        total_non_zeros = unique_word_cnt + unique_pairs
+        row_index = np.zeros(total_non_zeros).astype('int')
+        col_index = np.zeros(total_non_zeros).astype('int')
+        idx_vals = np.zeros(total_non_zeros)
+
+        idx = 0
+        for i in xrange(unique_word_cnt):
+            cnt = cnts[i]
+            # if cnt = 0, val will be 0. don't need to set index
+            if cnt > 1:
+                row_index[idx] = ids[i]
+                col_index[idx] = ids[i]
+                idx_vals[idx] = cnt * (cnt - 1)
+            idx += 1
+
+        for i in xrange(unique_word_cnt):
+            for j in xrange(i + 1, unique_word_cnt):
+                row_index[idx] = ids[i]
+                col_index[idx] = ids[j]
+                idx_vals[idx] = cnts[i] * cnts[j]
+                idx += 1
+        idx_vals /= (words_cnt * (words_cnt - 1))
+        rows.append(row_index)
+        cols.append(col_index)
+        vals.append(idx_vals)
+
+        # merge for each batch
+        if idx_d % batch_size == 0:
+            r = np.hstack(rows)
+            c = np.hstack(cols)
+            data = np.hstack(vals)
+            e2 += sp.coo_matrix((data, (r, c)), shape=(n_features, n_features))
+            # reset
+            rows = []
+            cols = []
+            vals = []
+
+    # last batch
+    if len(rows) > 0:
+        r = np.hstack(rows)
+        c = np.hstack(cols)
+        data = np.hstack(vals)
+        e2 += sp.coo_matrix((data, (r, c)), shape=(n_features, n_features))
+
+    # add symmetric pairs to lower triangle
+    e2 /= (n_samples - ignored_docs)
+    e2 += sp.triu(e2, k=1).T
+    return (e2, ignored_docs)
--- tensor_lda/tests/test_moments.py
+++ tensor_lda/tests/test_moments.py
@@ -8,7 +8,10 @@ from sklearn.utils.testing import assert_almost_equal
 from sklearn.utils.testing import assert_raises_regexp
 from sklearn.utils.testing import assert_greater
 
-from tensor_lda.moments import first_order_moments
+from sklearn.externals.six.moves import xrange
+
+from tensor_lda.moments import (first_order_moments,
+                                cooccurrence_expectation)
 
 
 def test_first_order_moments():
@@ -37,9 +40,9 @@ def test_first_order_moments_with_ignored_count():
     word_cnts = doc_word_mtx.sum(axis=1).astype('float')
     min_count = int(word_cnts.min() + 1)
     mask = (word_cnts >= min_count)
-    
+
     result = (doc_word_mtx / word_cnts[:, np.newaxis])[mask, :].sum(axis=0)
-    result /= n_samples
+    result /= mask.sum()
     m1, ignored_cnt = first_order_moments(doc_word_mtx, min_words=min_count,
                              whom='test_first_order_moments')
     assert_greater(ignored_cnt, 0)
@@ -51,3 +54,64 @@ def test_first_order_moments_with_ignored_count():
                         min_words=min_count, whom='test_first_order_moments')
     assert_equal(ignored_cnt, ignored_cnt_2)
     assert_array_almost_equal(m1, m1_2)
+
+
+def test_cooccurrence_expectation_simple():
+
+    doc_word_mtx = np.array([
+        [1, 0, 3, 2],
+        [4, 1, 5, 0]
+    ])
+
+    result_1 = np.array([
+        [0, 0, 3, 2],
+        [0, 0, 0, 0],
+        [3, 0, 6, 6],
+        [2, 0, 6, 2],
+    ])
+
+    result_2 = np.array([
+        [12, 4, 20, 0],
+        [4, 0, 5, 0],
+        [20, 5, 20, 0],
+        [0, 0, 0, 0],
+    ])
+
+    result = (result_1 / float(6 * 5)) + \
+             (result_2 / float(10 * 9))
+    result /= 2
+    e2, ignored_cnt = cooccurrence_expectation(doc_word_mtx, min_words=3,
+                             whom='test_cooccurrence_expectation')
+
+    assert_equal(ignored_cnt, 0)
+    assert_array_almost_equal(result, e2.toarray())
+
+
+def test_cooccurrence_expectation():
+    rng = np.random.RandomState(0)
+
+    n_features = 100
+    n_samples = rng.randint(100, 200)
+    doc_word_mtx = rng.randint(0, 3, size=(n_samples, n_features)).astype('float')
+
+    word_cnts = doc_word_mtx.sum(axis=1).astype('float')
+    min_count = int(word_cnts.min() + 1)
+    mask = (word_cnts >= min_count)
+
+    result = np.zeros((n_features, n_features))
+    for i in xrange(n_samples):
+        cnt = word_cnts[i]
+        if cnt < min_count:
+            continue
+        doc_i = doc_word_mtx[i, :]
+        result_i = (doc_i * doc_i[:, np.newaxis]) - np.diag(doc_i)
+        result_i /= cnt * (cnt - 1)
+        result += result_i
+    result /= mask.sum()
+
+    e2, ignored_cnt = cooccurrence_expectation(doc_word_mtx, min_words=min_count,
+                             whom='test_cooccurrence_expectation')
+
+    assert_greater(ignored_cnt, 0)
+    assert_equal(mask.sum(), n_samples - ignored_cnt)
+    assert_array_almost_equal(result, e2.toarray())
