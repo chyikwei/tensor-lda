19de853486816e729580322db7cca30857619b68
Chyi-Kwei Yau <chyikwei.yau@gmail.com>
2018-01-09 12:35:21 -0500

    add LDA sample generator

---
+++ tensor_lda/utils/sample_generator.py
@@ -0,0 +1,98 @@
+"""Generate samples dataset
+"""
+
+import numpy as np
+from scipy.sparse import coo_matrix
+from scipy.stats import poisson
+
+from sklearn.utils import check_random_state
+from sklearn.externals.six.moves import xrange
+
+
+class LdaSampleGenerator(object):
+    """Generate LDA samples
+
+    Parameters
+    ----------
+    n_topics : int
+        Number of topics
+    
+    n_words : int
+        Number of words in corpus
+
+    min_doc_size : int
+        Min word count in a document
+    
+    mean_doc_size : int
+        Mean word count in a document
+    
+    doc_topic_prior : double
+        Uniform Dirichlet prior of a document
+    
+    topic_word_prior : double
+        Uniform Dirichlet prior of a topic
+
+    mean_doc_size: int
+        Mean Value if word count in each document
+
+    Attributes
+    ----------
+    topic_word_distr_ : array, [n_topics, n_words]
+        parameters for topic word distribution.
+
+    """
+
+    def __init__(self, n_topics, n_words, min_doc_size,
+                 mean_doc_size, doc_topic_prior,
+                 topic_word_prior, random_state=None):
+        self.n_topics = n_topics
+        self.n_words = n_words
+        self.min_doc_size = min_doc_size
+        self.mean_doc_size = mean_doc_size
+        self.doc_topic_prior = doc_topic_prior
+        self.topic_word_prior = topic_word_prior
+        self.random_state = random_state
+
+        self.random_state_ = check_random_state(self.random_state)
+        # hidden variables
+        self.topic_word_prior_ = np.repeat(topic_word_prior, n_words)
+        # (n_topics, n_words)
+        self.doc_topic_prior_ = np.repeat(self.doc_topic_prior, n_topics)
+        self.topic_word_distr_ = self.random_state_.dirichlet(
+            self.topic_word_prior_, n_topics)
+
+    def generate_documents(self, n_docs):
+        """Generate Random doc-words Matrix
+
+        Parameters
+        ----------
+        n_docs : int
+            number of documents
+
+        Return
+        ------
+        doc_word_mtx : sparse matrix, [n_docs, n_words]
+            document words matrix
+
+        """
+        rs = self.random_state_
+        n_topics = self.n_topics
+        n_words = self.n_words
+        docs_size = poisson.rvs(mu=(self.mean_doc_size - self.min_doc_size),
+                                size=n_docs, random_state=rs)
+        docs_size += self.min_doc_size
+        doc_prior = np.repeat(self.doc_topic_prior, n_topics)
+        # (n_docs, n_topics)
+        docs_distr = rs.dirichlet(doc_prior, n_docs)
+
+        rows = []
+        cols = []
+        for i in xrange(n_docs):
+            word_dist = np.dot(self.topic_word_distr_.T, docs_distr[i, :])
+            word_idx = rs.choice(n_words, docs_size[i], p=word_dist, replace=True)
+            rows = np.append(rows, np.repeat(i, docs_size[i]))
+            cols = np.append(cols, word_idx)
+        data = np.ones(len(rows))
+        doc_word_mtx = coo_matrix((data, (rows, cols)),
+                                  shape=(n_docs, n_words)).tocsr()
+        return docs_distr, doc_word_mtx
---
+++ tensor_lda/utils/tests/test_sample_generator.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from sklearn.externals.six.moves import xrange
+from sklearn.utils.testing import assert_equal
+from sklearn.utils.testing import assert_true
+from sklearn.utils.testing import assert_array_equal
+from sklearn.utils.testing import assert_array_almost_equal
+from sklearn.utils.testing import assert_almost_equal
+
+from tensor_lda.utils.sample_generator import LdaSampleGenerator
+
+
+def test_lda_sample_generator():
+    n_topics = 20
+    n_words = 500
+    mean_words = 10
+    min_words = 3
+    doc_topic_prior = 1.
+    topic_word_prior = 1.
+    n_doc = 10000
+
+    gen = LdaSampleGenerator(n_topics=n_topics, n_words=n_words,
+                             min_doc_size=min_words,
+                             mean_doc_size=mean_words,
+                             doc_topic_prior=doc_topic_prior,
+                             topic_word_prior=topic_word_prior,
+                             random_state=0)
+
+    docs_distr, doc_word_mtx = gen.generate_documents(n_doc)
+    # check shape
+    assert_equal((n_doc, n_topics), docs_distr.shape)
+    assert_equal((n_doc, n_words), doc_word_mtx.shape)
+
+    # check word count
+    word_cnts = doc_word_mtx.sum(axis=1)
+    assert_equal(word_cnts.shape[0], n_doc)
+    assert_true(np.all(word_cnts >= min_words))
+    assert_true(abs(word_cnts.mean() - mean_words) < 0.1)
+
+    # check doc distribution
+    assert_array_almost_equal(np.ones(n_doc), docs_distr.sum(axis=1))
+    doc_dirichelet_mean = gen.doc_topic_prior_ / float(gen.doc_topic_prior_.sum())
+    assert_array_almost_equal(doc_dirichelet_mean, docs_distr.mean(axis=0), decimal=3)
+
+    # check topic distr
+    topic_distr = gen.topic_word_distr_
+    assert_equal((n_topics, n_words), topic_distr.shape)
+    assert_array_almost_equal(np.ones(n_topics), topic_distr.sum(axis=1))
+    word_dirichelet_mean = gen.topic_word_prior_ / float(gen.topic_word_prior_.sum())
+    # set decimal to 2 since variance is large
+    assert_array_almost_equal(word_dirichelet_mean, topic_distr.mean(axis=0), decimal=2)
