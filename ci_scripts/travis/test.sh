e9c606fc22ed6f1cf08cd55ff643493122f04bff
Chyi-Kwei Yau <chyikwei.yau@gmail.com>
2021-01-06 22:54:29 -0800

    re-enable coverage (#10)

--- ci_scripts/travis/test.sh
+++ ci_scripts/travis/test.sh
@@ -2,12 +2,7 @@ set -e
 
 # Get into a temp directory to run test from the installed scikit learn and
 # check if we do not leave artifacts
-mkdir -p $TEST_DIR
+mkdir -p $TEST_DIR/
 
-cd $TEST_DIR
-
-if [[ "$COVERAGE" == "true" ]]; then
-    nosetests -s --with-coverage --cover-package=$MODULE $MODULE
-else
-    nosetests -s $MODULE
-fi
+cd $TEST_DIR/
+nosetests -s --with-coverage --cover-package=$MODULE $MODULE
